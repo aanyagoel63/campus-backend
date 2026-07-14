9545d4f1efd03cca1fd736302ff3fcde616425f5
aanyagoel63 <aanyagoel6393@gmail.com>
2026-07-14 10:16:18 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Aanya-goel02",
-  database: "campusverse"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
