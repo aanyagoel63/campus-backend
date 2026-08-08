69b38aaf365f7ad6f23357af1259dbb8f6a28598
aanyagoel63 <aanyagoel6393@gmail.com>
2026-08-08 12:20:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -132,8 +132,8 @@ app.post("/contact", (req, res) => {
 // ==============================
 console.log("Reached app.listen");
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
